c0403e5b4fa2ce78bc42451b4c8374a0e0f0d76e
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-20 15:34:27 +0530

    Update app.init.js

---
+++ js/core/app.init.js
@@ -0,0 +1,7 @@
+import { initHeader } from '../ui/header.ui.js';
+import { initTabs } from '../ui/tabs.ui.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    initHeader();
+    initTabs();
+});
