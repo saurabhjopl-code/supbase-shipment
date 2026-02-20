79699c6f7620e2037fa071aa4f9488c6270683c5
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-20 15:49:38 +0530

    Update app.init.js

--- js/core/app.init.js
+++ js/core/app.init.js
@@ -1,7 +1,15 @@
-import { initHeader } from '../ui/header.ui.js';
-import { initTabs } from '../ui/tabs.ui.js';
+import { renderTabs } from '../ui/tabs.ui.js'
+import { initTabBehavior } from '../ui/layout.controller.js'
+import { fetchCounts } from '../data/data.fetch.js'
 
-document.addEventListener("DOMContentLoaded", () => {
-    initHeader();
-    initTabs();
-});
+document.addEventListener("DOMContentLoaded", async () => {
+
+    renderTabs()
+    initTabBehavior()
+
+    document.getElementById("refreshBtn")
+        .addEventListener("click", fetchCounts)
+
+    await fetchCounts()
+
+})
