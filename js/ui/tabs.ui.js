bbfb2255c23b97cd1ae00339001d68a7e54aa6e6
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-20 15:48:06 +0530

    Update tabs.ui.js

---
+++ js/ui/tabs.ui.js
@@ -0,0 +1,11 @@
+export function renderTabs() {
+    const container = document.getElementById("mpTabs")
+
+    container.innerHTML = `
+        <button class="tab active" data-tab="summary">Summary</button>
+        <button class="tab" data-tab="amazon">Amazon</button>
+        <button class="tab" data-tab="flipkart">Flipkart</button>
+        <button class="tab" data-tab="myntra">Myntra</button>
+        <button class="tab" data-tab="seller">SELLER</button>
+    `
+}
