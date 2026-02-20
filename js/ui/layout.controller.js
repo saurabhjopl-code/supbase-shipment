bbdcc35d3cadf3bd1be3162e9fc5c984df2b7a01
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-20 16:11:20 +0530

    Update layout.controller.js

--- js/ui/layout.controller.js
+++ js/ui/layout.controller.js
@@ -1,3 +1,6 @@
+import { renderSummary } from '../summary/summary.base.js'
+import { renderReport } from '../reports/report.base.js'
+
 export function initTabBehavior() {
 
     const tabs = document.querySelectorAll(".tab")
@@ -5,18 +8,23 @@ export function initTabBehavior() {
     const reportSection = document.getElementById("reportSection")
 
     tabs.forEach(tab => {
-        tab.addEventListener("click", () => {
+        tab.addEventListener("click", async () => {
 
             tabs.forEach(t => t.classList.remove("active"))
             tab.classList.add("active")
 
-            if (tab.dataset.tab === "summary") {
+            const tabName = tab.dataset.tab
+
+            if (tabName === "summary") {
                 summarySection.classList.remove("hidden")
                 reportSection.classList.add("hidden")
+                await renderSummary()
             } else {
                 summarySection.classList.add("hidden")
                 reportSection.classList.remove("hidden")
+                await renderReport(tabName)
             }
+
         })
     })
 }
