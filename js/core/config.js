a40c0c541e8dc498df3411662c57e6350f736a56
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-20 15:47:34 +0530

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,5 +1,7 @@
 export const CONFIG = {
-  TARGET_SC: 45,
-  RECALL_SC: 90,
-  DAYS: 30
-};
+    SUPABASE_URL: "https://nklubwhydqxldtwxxrzr.supabase.co",
+    SUPABASE_KEY: "sb_publishable_cRlyT9eZ53LTmogyNd4kGg_n-ld8BnZ",
+    TARGET_SC: 45,
+    RECALL_SC: 90,
+    DAYS: 30
+}
